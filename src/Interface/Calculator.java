b121efe801da145e9bfa698c5c6d05c1d85ab900
Deth <Deth@Deth>
2015-12-27 21:52:46 +0200

    add some buttons and forms

--- src/Interface/Calculator.java
+++ src/Interface/Calculator.java
@@ -6,10 +6,26 @@ import javax.swing.JFrame;
 import javax.swing.JToolBar;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenu;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Calculator {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -39,14 +55,233 @@ public class Calculator {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("\u041A\u0430\u043B\u044C\u043A\u0443\u043B\u044F\u0442\u043E\u0440");
+		frame.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+		});
+		frame.setBounds(100, 100, 303, 316);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JToolBar toolBar = new JToolBar();
-		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
+		JPanel panelVisible = new JPanel();
+		frame.getContentPane().add(panelVisible, BorderLayout.NORTH);
+		panelVisible.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		textField = new JTextField();
+		textField.setColumns(20);
+		textField.setText("0");
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField.setEditable(false);
+		panelVisible.add(textField);
+		
+		JPanel panelButtons = new JPanel();
+		
+		frame.getContentPane().add(panelButtons, BorderLayout.CENTER);
+		panelButtons.setLayout(null);
+		// Zapominanie
+		JButton btnNewButton = new JButton("MC");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(10, 22, 60, 23);
+		panelButtons.add(btnNewButton);
+		
+		JButton btnMr = new JButton("MR");
+		btnMr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnMr.setBounds(80, 22, 60, 23);
+		panelButtons.add(btnMr);
+		
+		JButton btnM = new JButton("M+");
+		btnM.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnM.setBounds(150, 22, 60, 23);
+		panelButtons.add(btnM);
+		
+		JButton btnM_1 = new JButton("M-");
+		btnM_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnM_1.setBounds(220, 22, 60, 23);
+		panelButtons.add(btnM_1);
+		// konec zapominanie
+		// Znaki deystviya
+		
+		JButton button = new JButton("/");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button.setBounds(220, 102, 60, 23);
+		panelButtons.add(button);
+		
+		JButton btnX = new JButton("X");
+		btnX.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnX.setBounds(220, 136, 60, 23);
+		panelButtons.add(btnX);
+		
+		JButton button_2 = new JButton("-");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_2.setBounds(220, 170, 60, 23);
+		panelButtons.add(button_2);
+		
+		JButton button_3 = new JButton("+");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_3.setBounds(220, 204, 60, 23);
+		panelButtons.add(button_3);
+		
+		// konec znakov
+		
+		// Panel cifr
+		JButton button_1 = new JButton("7");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_1.setBounds(10, 102, 60, 23);
+		panelButtons.add(button_1);
+		
+		JButton button_4 = new JButton("8");
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_4.setBounds(80, 102, 60, 23);
+		panelButtons.add(button_4);
+		
+		JButton button_5 = new JButton("9");
+		button_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_5.setBounds(150, 102, 60, 23);
+		panelButtons.add(button_5);
+		
+		JButton button_6 = new JButton("6");
+		button_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_6.setBounds(150, 136, 60, 23);
+		panelButtons.add(button_6);
+		
+		JButton button_7 = new JButton("5");
+		button_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_7.setBounds(80, 136, 60, 23);
+		panelButtons.add(button_7);
+		
+		JButton button_8 = new JButton("4");
+		button_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_8.setBounds(10, 136, 60, 23);
+		panelButtons.add(button_8);
+		
+		JButton button_9 = new JButton("3");
+		button_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_9.setBounds(150, 170, 60, 23);
+		panelButtons.add(button_9);
+		
+		JButton button_10 = new JButton("2");
+		button_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_10.setBounds(80, 170, 60, 23);
+		panelButtons.add(button_10);
+		
+		JButton button_11 = new JButton("1");
+		button_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_11.setBounds(10, 170, 60, 23);
+		panelButtons.add(button_11);
+		
+		JButton button_12 = new JButton("0");
+		button_12.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_12.setBounds(80, 204, 60, 23);
+		panelButtons.add(button_12);
+		// konec cifr
+		
+		// ZApyataya, smena znaka, ravno
+		
+		JButton button_13 = new JButton(",");
+		button_13.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_13.setBounds(10, 204, 60, 23);
+		panelButtons.add(button_13);
+		
+		JButton button_14 = new JButton("\u2213");
+		button_14.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_14.setBounds(220, 56, 60, 23);
+		panelButtons.add(button_14);
+		
+		JButton button_15 = new JButton("=");
+		button_15.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		button_15.setBounds(150, 204, 60, 23);
+		panelButtons.add(button_15);
+		// konec ZApyataya, smena znaka, ravno
+		
+		// Ochistit` poslednee, ochistit` vse, backspace
+		JButton btnCe = new JButton("CE");
+		btnCe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCe.setBounds(10, 56, 60, 23);
+		panelButtons.add(btnCe);
+		
+		JButton btnC = new JButton("C");
+		btnC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnC.setBounds(80, 56, 60, 23);
+		panelButtons.add(btnC);
+		
+		JButton button_16 = new JButton("\u21D0");
+		button_16.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_16.setBounds(150, 56, 60, 23);
+		panelButtons.add(button_16);
+		// konec Ochistit` poslednee, ochistit` vse, backspace
 	}
-
 }
